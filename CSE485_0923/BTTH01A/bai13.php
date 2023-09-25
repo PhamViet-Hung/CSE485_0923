171fa3aaa7769015ad15be62e7be113f27edadcb
PhamViet-Hung <138470428+PhamViet-Hung@users.noreply.github.com>
2023-09-25 22:33:29 +0700

    Update bai13.php

--- CSE485_0923/BTTH01A/bai13.php
+++ CSE485_0923/BTTH01A/bai13.php
@@ -1,5 +1,30 @@
 <?
-echo "A";
-echo "Dòng 1\n Dòng 2\n Dòng 3"; // Sử dụng ký tự escape \n
-echo "Dòng 1" . PHP_EOL . "Dòng 2" . PHP_EOL . "Dòng 3"; // Sử dụng hàm PHP_EOL
+$numbers = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72,
+65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
+
+// Tính giá trị trung bình của mảng
+$average = array_sum($numbers) / count($numbers);
+
+// Liệt kê các số lớn hơn giá trị trung bình
+$greaterThanAverage = [];
+foreach ($numbers as $number) {
+    if ($number > $average) {
+        $greaterThanAverage[] = $number;
+    }
+}
+
+// Liệt kê các số nhỏ hơn hoặc bằng giá trị trung bình
+$lessThanOrEqualAverage = [];
+foreach ($numbers as $number) {
+    if ($number <= $average) {
+        $lessThanOrEqualAverage[] = $number;
+    }
+}
+
+// Hiển thị kết quả
+echo "Giá trị trung bình của mảng: " . $average . "\n";
+echo "Các số lớn hơn giá trị trung bình: ";
+print_r($greaterThanAverage);
+echo "Các số nhỏ hơn hoặc bằng giá trị trung bình: ";
+print_r($lessThanOrEqualAverage);
 ?>
